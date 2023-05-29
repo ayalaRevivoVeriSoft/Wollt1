8f3ec6c88e826deedb0a86e8ecda272ec8c2c75a
ayala revivo <ar0527167617@gmail.com>
2023-05-29 17:50:33 +0300

    Update test2.js

--- test/test2.js
+++ test/test2.js
@@ -1,33 +1,36 @@
-//7
+//5
+web.transaction('01')
 web.init();
 web.open('https://wolt.com/he/discovery');
 web.click('div.sc-3acf7193-2.kUcLXW>button.sc-eda0895a-2.kcvQDI')
 web.pause(2000)
 web.selectWindow('title=בואו לגלות עולם שלם של מסעדות & חנויות ב-Wolt – Wolt');
-
+web.transaction('01')
 web.click("//*[contains(text(),'כניסה')]")
 
 
 web.click("//*[contains(text(),'כניסה דרך גוגל')]")
-
+web.transaction('03')
 web.pause(2000)
 web.type('id=identifierId', 'ar0527167617@gmail.com')
-
+web.transaction('04')
 web.click("//span[contains(text(),'הבא') and @class='VfPpkd-vQzf8d']")
 
 web.type("name=Passwd","AYALAREVIVO1!")
 web.pause(2000)
+web.transaction('05')
 web.click("//span[contains(text(),'הבא') and @class='VfPpkd-vQzf8d']")
 web.pause(10000)
-
+web.transaction('06')
 web.click("//*[contains(text(),'Gift Card')]");
 web.click("//*[contains(text(),'גיפט קארד - 45')]");
 web.click("//*[contains(text(),'להוסיף להזמנה')]");
-
+web.transaction('07')
 web.pause(5000)
 web.click("//*[contains(text(),'לצפייה בהזמנ')]");
 
 web.click("//*[contains(text(),'מעבר לתשלום')]");
+web.transaction('08')
 
 web.pause(5000)
 web.click("/html/body/div[2]/div[2]/main/div[5]/div[2]/div[1]/ul[2]/li");
@@ -54,16 +57,16 @@ web.type("//*[@id='txtPassword']","AYALAREVIVO1!");
 web.click('id=btnSubmit');
 
 
-// web.click('id=btnPay');
+web.click('id=btnPay');
 
 web.transaction('10')
 
 
 if(web.isExist('//*[contains(text(),"יש לך מייל")]',60))
     log.info("wow");
-else{
+if(web.isExist('//*[contains(text(),"אישור העסקה לא הצלי")]',60))
     log.info("oooof")
-}
+
 
 
 
